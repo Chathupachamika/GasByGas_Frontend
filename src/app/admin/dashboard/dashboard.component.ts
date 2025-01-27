a975d99288c67ba5fab4ccff0d30326852b9b9a6
Chathupa Chamika <168677571+Chathupachamika@users.noreply.github.com>
2025-01-27 15:51:16 +0530

    update

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HeaderComponent } from "../../common/header/header.component";
+import { SidebarComponent } from "../../common/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [SidebarComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA] // added to recognize custom elements
