5114166e4ed78287f7038a9d8a6595930504b347
Chathupa Chamika <168677571+Chathupachamika@users.noreply.github.com>
2025-01-27 16:30:30 +0530

    update commit

--- src/app/common/sidebar/sidebar.component.ts
+++ src/app/common/sidebar/sidebar.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../../service/login.service';
 
 @Component({
   selector: 'app-sidebar',
   imports: [],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrls: ['./sidebar.component.css'] // Corrected property name
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit { // Added 'implements' keyword
+  userDetails: any = null;
 
+  constructor(private loginService: LoginService) {}
+
+  ngOnInit(): void {
+    if (this.loginService.isLoggedIn()) {
+      const userDetailsStr = localStorage.getItem('userDetails');
+      if (userDetailsStr) {
+        this.userDetails = JSON.parse(userDetailsStr);
+        console.log('Logged in user details:', this.userDetails);
+      }
+    }
+  }
 }
